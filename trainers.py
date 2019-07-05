9f874c160751eff3f33beaffa6551ae75bbcf5e4
Lukoshkin Vlad WX757276 <lukoshkin.vlad@huawei.com>
2019-07-05 21:06:47 +0300

    Start reworking trainers.py

--- trainers.py
+++ trainers.py
@@ -14,12 +14,10 @@ if torch.cuda.is_available():
 else:
     T = torch
 
-
 def images_to_numpy(tensor):
     generated = (torch.clamp(tensor, -1, 1) + 1) / 2 * 255
     generated = generated.data.cpu().numpy().transpose(0, 2, 3, 1)
     return generated.astype('uint8')
-
 
 def videos_to_numpy(tensor):
     generated = (torch.clamp(tensor, -1, 1) + 1) / 2 * 255
@@ -33,7 +31,7 @@ class Trainer(object):
 
         self.use_categories = use_categories
 
-        self.gan_criterion = nn.BCEWithLogitsLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
         self.category_criterion = nn.CrossEntropyLoss()
 
         self.image_sampler = image_sampler
@@ -48,120 +46,60 @@ class Trainer(object):
         self.log_folder = Path(log_folder)
 
         self.use_cuda = use_cuda
-        self.use_infogan = use_infogan
 
-        self.image_enumerator = None
-        self.video_enumerator = None
 
-    @staticmethod
-    def create_by_shape(tensor, val):
-        return T.FloatTensor(tensor.size()).fill_(val) 
+        torch.log(scores).mean()
+        torch.log1p(-scores).mean()
 
-    def sample_real_image_batch(self):
-        if self.image_enumerator is None:
-            self.image_enumerator = enumerate(self.image_sampler)
+    def train_discriminator(self):
+        gen_pairs = composeGenerated()
+        neg_pairs = sampleWrongAssociations()
+        pos_pairs = sampleCorrectAssociations()
 
-        batch_idx, batch = next(self.image_enumerator)
-        b = batch
-        if self.use_cuda:
-            for k, v in batch.items():
-                b[k] = v.cuda()
+        pos_scores = discriminator(pos_pairs)
+        neg_scores = discriminator(neg_pairs)
+        gen_scores = discriminator(gen_pairs) # fake_batch.detach()
 
-        if batch_idx == len(self.image_sampler) - 1:
-            self.image_enumerator = enumerate(self.image_sampler)
 
-        return b
-
-    def sample_real_video_batch(self):
-        if self.video_enumerator is None:
-            self.video_enumerator = enumerate(self.video_sampler)
-
-        batch_idx, batch = next(self.video_enumerator)
-        b = batch
-        if self.use_cuda:
-            for k, v in batch.items():
-                b[k] = v.cuda()
-
-        if batch_idx == len(self.video_sampler) - 1:
-            self.video_enumerator = enumerate(self.video_sampler)
-
-        return b
-
-    def train_discriminator(self, discriminator, 
-                            sample_true, sample_fake, 
-                            opt, batch_size, use_categories):
-        opt.zero_grad()
-
-        real_batch = sample_true()
-        batch = real_batch['values']
-
-        fake_batch, generated_categories = sample_fake(batch_size)
-
-        real_labels, real_categorical = discriminator(batch)
-        fake_labels, fake_categorical = discriminator(fake_batch.detach())
-
-        ones = self.create_by_shape(real_labels, 1)
-        zeros = self.create_by_shape(fake_labels, 0)
-
-        l_discriminator = self.gan_criterion(real_labels, ones) + \
-                          self.gan_criterion(fake_labels, zeros)
-
-        if use_categories:
-            # Ask the video discriminator to learn categories from training videos
-            categories_gt = torch.squeeze(real_batch['categories'].long())
-            l_discriminator += self.category_criterion(real_categorical.squeeze(), categories_gt)
-
-        l_discriminator.backward()
-        opt.step()
 
         return l_discriminator
 
-    def train_generator(self,
-                        image_discriminator, video_discriminator,
+    def train_generator(self, im_dis, vi_dis,
                         sample_fake_images, sample_fake_videos,
                         opt):
-
         opt.zero_grad()
 
         # train on images
-
-        fake_batch, generated_categories = sample_fake_images(self.image_batch_size)
-        fake_labels, fake_categorical = image_discriminator(fake_batch)
+        fake_batch = sample_fake_images(self.image_batch_size)
+        fake_labels = image_discriminator(fake_batch)
         all_ones = self.create_by_shape(fake_labels, 1)
 
         l_generator = self.gan_criterion(fake_labels, all_ones)
 
         # train on videos
-
-        fake_batch, generated_categories = sample_fake_videos(self.video_batch_size)
-        fake_labels, fake_categorical = video_discriminator(fake_batch)
+        fake_batch = sample_fake_videos(self.video_batch_size)
+        fake_labels = video_discriminator(fake_batch)
         all_ones = self.create_by_shape(fake_labels, 1)
 
         l_generator += self.gan_criterion(fake_labels, all_ones)
-
-        if self.use_infogan:
-            # Ask the generator to generate categories recognizable by the discriminator
-            l_generator += self.category_criterion(fake_categorical.squeeze(), generated_categories)
-
         l_generator.backward()
         opt.step()
 
         return l_generator
 
-    def train(self, generator, image_discriminator, video_discriminator):
+    def train(self, gen, im_dis, vi_dis):
         if self.use_cuda:
-            generator.cuda()
-            image_discriminator.cuda()
-            video_discriminator.cuda()
+            gen.cuda()
+            im_dis.cuda()
+            vi_dis.cuda()
 
         logger = Logger(self.log_folder)
 
         # create optimizers
-        opt_generator = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999), weight_decay=0.00001)
-        opt_image_discriminator = optim.Adam(image_discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999),
-                                             weight_decay=0.00001)
-        opt_video_discriminator = optim.Adam(video_discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999),
-                                             weight_decay=0.00001)
+        opt_gen  = optim.Adam(gen.parameters(), lr=2e-4, betas=(.5, .999), weight_decay=1e-5)
+
+        opt_imdis = optim.Adam(im_dis.parameters(), lr=2e-4, betas=(.5, .999), weight_decay=1e-5)
+        opt_vidis = optim.Adam(vi_dis.parameters(), lr=2e-4, betas=(.5, .999), weight_decay=1e-5)
 
         # training loop
 
@@ -190,12 +128,12 @@ class Trainer(object):
             opt_video_discriminator.zero_grad()
 
             # train image discriminator
-            l_image_dis = self.train_discriminator(image_discriminator, self.sample_real_image_batch,
+            l_idis = self.train_discriminator(image_discriminator, self.sample_real_image_batch,
                                                    sample_fake_image_batch, opt_image_discriminator,
                                                    self.image_batch_size, use_categories=False)
 
             # train video discriminator
-            l_video_dis = self.train_discriminator(video_discriminator, self.sample_real_video_batch,
+            l_vdis = self.train_discriminator(video_discriminator, self.sample_real_video_batch,
                                                    sample_fake_video_batch, opt_video_discriminator,
                                                    self.video_batch_size, use_categories=self.use_categories)
 
