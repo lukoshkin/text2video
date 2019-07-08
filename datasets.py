2bebb0612e05b55f2fdb0047f85f1df3403ebfc2
Lukoshkin Vlad WX757276 <lukoshkin.vlad@huawei.com>
2019-07-08 18:23:52 +0300

    Debug datasets.py

--- datasets.py
+++ datasets.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 import cv2
 import numpy as np
@@ -9,83 +10,68 @@ from torch.utils.data import Dataset
 
 class VideoDataset(Dataset):
     def __init__(self, path, cache, 
-                 step=1, transform=None, ext='webm'):
+                 video_shape=(32, 64, 64, 3), 
+                 step=2, transform=None, ext='webm'):
         self.transform = transform
 
         path = Path(path)
         file_name = path.stem.split('.')[0]
 
         cache = Path(cache)
-        if (cache / file_name).exists():
-            self.data = np.load(cache / file_name)
+        if (cache / f"{file_name}.db").exists():
+            with open(cache / f"{file_name}.db", 'rb') as fp:
+                self.data = pickle.load(fp)
         else:
             self.data = []
-            durations = []
             cache.mkdir(parents=True, exist_ok=True)
 
-            with open(path) as fh:
-                raw_data = json.load(fh)
+            with open(path) as fp:
+                raw_data = json.load(fp)
             
+            mult = []
+            corrupted = 0
+            D, H, W, C = video_shape
             folder = path.parents[0]
             for sample in tqdm(raw_data, "Preparing dataset"):
                 video = folder / f"{sample['id']}.{ext}"
-                ViCap = cv2.VideoCapture(video)
+                ViCap = cv2.VideoCapture(str(video))
+                _D = ViCap.get(cv2.CAP_PROP_FRAME_COUNT)
 
+                if int(_D) <  D :  continue
+                mult.append(int(_D) // D) 
+
+                CNT = 0
                 frames = []
                 success = True
-                while success:
+                while success and (CNT < D * mult[-1]):
                     success, image = ViCap.read()
                     if success:
+                        image = cv2.resize(image, (H, W))
                         frames += [image]
+                        CNT += 1
 
                 ViCap.release()
                 cv2.destroyAllWindows()
-                frames = np.array(frames, 'uint8')
 
-                assert len(frames) > 0, \
-                "Something went wrong, no frames were extracted"
-                durations.append(len(frames)) 
-                self.data.append((sample['label'], frames))
-            
-            durations = np.array(durations, 'uint16')
+                if CNT == D * mult[-1]:
+                    frames = np.array(frames, 'uint8')
+                    self.data.append (
+                        (  sample['label'], 
+                           frames[::step * mult[-1]]  )
+                    )
+                else:
+                    corrupted += 1
+                    mult.pop()
 
-            vlen = durations.min()
-            N = vlen // step + 1
-            self.data = np.rec.array (
-                [(x, y[:vlen:step]) for x, y in self.data],
-                [('', 'O'), ('', 'uint8', (N, *image.shape)]
-            )
-            np.savez (
-                cache / f'{file_name}.db', 
-                self.data, durations
-            )
+            print('No of corrupted videos', corrupted)
+            with open(cache / f'{file_name}.db', 'wb') as fp:
+                pickle.dump(self.data, fp)
 
     def __getitem__(self, index):
-        return self.data[index]
+        label, video = self.data[index]
+
+        return {'label' : label, 
+                'video' : video}
 
     def __len__(self):
         return len(self.data)
-        
-
-
-class ImagesFromVideoDataset(Dataset):
-    def __init__(self, path, transform=None):
-        with open(path, 'rb') as fh:
-            self.data = np.load(fh)
-            self.scan_r = np.cumsum(np.load(fh))
-        self.scan_l = np.r_[0, self.scan_r[:-1]]    
-
-        self.transform = transform if transform else lambda x: x
-
-    def __getitem__(self, index):
-        vi_no = np.searchsorted(self.scan_r, index, 'right')
-        im_no = index - self.scan_l[vi_no] 
-        label, frames = self.data[vi_no]
-
-        # further, label may be changed to placeholders
-        return {'label'  : label, 
-                'values' : self.transform(frames[im_no])}
-
-    def __len__(self):
-        return self.scan_r[-1]
-
